4c2c69d3269c299125f3fabee779cad4186eca32
Shubham Sonawane <50284309+shubham1925@users.noreply.github.com>
2020-04-20 22:46:39 -0400

    Implement gaussian pyramid model

--- LKT_baby.py
+++ LKT_baby.py
@@ -22,11 +22,29 @@ def affine_matrix(p):
     W = np.array([[1, 0, 0], [0, 1, 0]]) + p.reshape((2,3), order = 'F')
     return W
 
+def gaussian_pyramid(img, tmp, levels = 3):
+    img_copy_down, tmp_down = img.copy(), tmp.copy()
+    img_copy_up, tmp_up = img.copy(), tmp.copy()
+    gaussian_pyr_img_down, gaussian_pyr_temp_down = [img_copy_down], [tmp_down]
+    gaussian_pyr_img_up, gaussian_pyr_temp_up = [img_copy_up], [tmp_up]
+    for i in range(levels):
+        img_copy_down = cv.pyrDown(img_copy_down)
+        img_copy_up = cv.pyrUp(img_copy_up)
+        gaussian_pyr_img_down.append(img_copy_down)
+        gaussian_pyr_img_up.append(img_copy_up)
+        
+        tmp_down = cv.pyrDown(tmp_down)
+        tmp_up = cv.pyrUp(tmp_up)
+        gaussian_pyr_temp_down.append(tmp_down)
+        gaussian_pyr_temp_up.append(tmp_up)
+    return gaussian_pyr_img_down, gaussian_pyr_img_up, gaussian_pyr_temp_down, gaussian_pyr_temp_up
+    
+
 def lucas_kanade_tracker(img, tmp, rect, p_prev):
     iterations = 0
     cost_function = 1
-    threshold = 0.1 #check different threshold
-    W = affine_matrix(p_prev)    
+    threshold = 0.001 #check different threshold
+    W = affine_matrix(p_prev) 
 
     while cost_function > threshold:
         iterations = iterations + 1
@@ -34,14 +52,15 @@ def lucas_kanade_tracker(img, tmp, rect, p_prev):
         #form the warped image  - Step 1
         Iw = cv.warpAffine(img, W, (0, 0), flags=cv.INTER_CUBIC + cv.WARP_INVERSE_MAP)#combination trial      
         Iw = Iw[rect[0,1]:rect[2,1] , rect[0,0]:rect[2,0]]
-        cv.imshow("warped", Iw)
-
+        
         #calculating error - step 2
         error = tmp.flatten().astype(np.int)-Iw.flatten().astype(np.int)
-
+#        error = error**2
+#        cv2.imshow("error", cv2.subtract(tmp_2, Iw_2))
+   
         #calculate the gradient of main image along x and y - step 3
-        grad_x = cv.Sobel(np.float32(img), cv.CV_64F, 1, 0, ksize =5) #changing kernel size improved results at start
-        grad_y = cv.Sobel(np.float32(img), cv.CV_64F, 0, 1, ksize =5)
+        grad_x = cv.Sobel(np.float32(img), cv.CV_64F, 1, 0, ksize =9) #changing kernel size improved results at start
+        grad_y = cv.Sobel(np.float32(img), cv.CV_64F, 0, 1, ksize =9)
         cv.imshow("y", grad_y)
         
         #calculating the warp of the gradient
@@ -85,10 +104,12 @@ def lucas_kanade_tracker(img, tmp, rect, p_prev):
 
         if(iterations > 1000):
             break
-#    print("iterations : " + str(iterations))
+    print("iterations : " + str(iterations))
     return p_prev
 
-vid_output = cv.VideoWriter('lkt_baby.avi', cv.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (images[0].shape[1], images[0].shape[0]))
+vid_output = cv.VideoWriter('lkt_baby_pyramid.avi', cv.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (images[0].shape[1], images[0].shape[0]))
+
+
 
 
 rect1 = np.array([[160,83], [216,83], [216,148], [160,148]])
@@ -97,9 +118,21 @@ rect2 = np.array([[152,118], [227,118], [227,236], [152,236]])
 
 rect3 = np.array([[182,65], [248,65], [248,115], [182,115]])
 
+rect1_pyramid_layer1 = np.array([[84,39], [107,39], [107,73], [84,73]])
+rect1_pyramid_layer2 = np.array([[42,21], [54,21], [54,34], [42,34]])
+rect1_pyramid_layer3 = np.array([[21,10], [27,10], [27,15], [21,15]])
+
+rect1_pyramid_layer1 = np.array([[84,39], [107,39], [107,73], [84,73]])
+rect1_pyramid_layer2 = np.array([[42,21], [54,21], [54,34], [42,34]])
+rect1_pyramid_layer3 = np.array([[21,10], [27,10], [27,15], [21,15]])
+
 param_1 = np.zeros(6) 
 param_2 = np.zeros(6)
 param_3 = np.zeros(6)
+
+p1_level = np.zeros(6)
+p2_level = np.zeros(6)
+p3_level = np.zeros(6)
 
 template1 = images[0][rect1[0,1]:rect1[2,1] , rect1[0,0]:rect1[2,0]]
 template2 = images[85][rect2[0,1]:rect2[2,1] , rect2[0,0]:rect2[2,0]]
@@ -107,35 +140,58 @@ template2 = images[85][rect2[0,1]:rect2[2,1] , rect2[0,0]:rect2[2,0]]
 rect_updated_1 = copy.deepcopy(rect1)
 rect_updated_2 = copy.deepcopy(rect2)
 
-for i in range(1,len(images)):
+for i in range(1,len(images)-1):
     print("Frame no : "+str(i))    
-    if i < 85:
-        param_1 = lucas_kanade_tracker(images[i], template1, rect1, param_1)        
-        w1 = affine_matrix(param_1)
+    if i < 120:
+        img_1 = images[0]
+        img_2 = cv.pyrDown(img_1)
+        img_3 = cv.pyrDown(img_2)
+        
+        img_1_next = images[i]
+        img_2_next = cv.pyrDown(img_1_next)
+        img_3_next = cv.pyrDown(img_2_next)
+        
+        #3rd layer
+        init_temp_0_0 = img_3
+        init_temp_1_0 = img_3_next
+        p1_level = lucas_kanade_tracker(init_temp_1_0, init_temp_0_0[rect1_pyramid_layer2[0,1]:rect1_pyramid_layer2[2,1] , 
+                                                                 rect1_pyramid_layer2[0,0]:rect1_pyramid_layer2[2,0]], 
+                                                                    rect1_pyramid_layer2, p1_level)
+        
+        
+        #2nd layer
+        init_temp_0_1 = img_2
+        init_temp_1_1 = img_2_next
+        p2_level = lucas_kanade_tracker(init_temp_1_1, init_temp_0_1[rect1_pyramid_layer1[0,1]:rect1_pyramid_layer1[2,1] , 
+                                                                 rect1_pyramid_layer1[0,0]:rect1_pyramid_layer1[2,0]],rect1_pyramid_layer1, p1_level*2)
+        
+        
+        #1st layer
+        init_temp_0_2 = img_1
+        init_temp_1_2 = img_1_next
+        p_temp = (p1_level*4 + p2_level*2)
+        cv.imshow("1", init_temp_0_2[rect1[0,1]:rect1[2,1], rect1[0,0]:rect1[2,0]])
+        p3_level = lucas_kanade_tracker(init_temp_1_2, init_temp_0_2[rect1[0,1]:rect1[2,1], rect1[0,0]:rect1[2,0]], rect1, p_temp)
+        
+        print(p3_level)
+        
+        rect_trial_updated = np.array([[160+int(p3_level[4]),83+int(p3_level[5])], [216+int(p3_level[4]),83+int(p3_level[5])], 
+                                       [216+int(p3_level[4]),148+int(p3_level[5])], [160+int(p3_level[4]),148+int(p3_level[5])]])
+       
+        w1 = affine_matrix(p3_level)
         #draw rectange as per updated parameters
         temp_rect_1 = np.dot(w1,np.vstack((rect1.T, np.ones((1,4))))).T
-        print(temp_rect_1)
+
         #extract corner elements
         [xmax1, ymax1] = list(np.max(temp_rect_1, axis = 0).astype(np.int))
         [xmin1, ymin1] = list(np.min(temp_rect_1, axis = 0).astype(np.int))
         #create new rectangle/bounding box
-        rect_updated_1=np.array([[xmin1,ymin1],[xmax1,ymin1],[xmax1,ymax1],[xmin1,ymax1]])        
+        rect_updated_1=np.array([[xmin1,ymin1],[xmax1,ymin1],[xmax1,ymax1],[xmin1,ymax1]]) 
+
         output = cv.polylines(original_frame[i],[rect_updated_1],True,(0,0,255),thickness = 2)
         vid_output.write(output)
         cv.imshow("tracker",output)
-    elif i >= 85:
-        param_2 = lucas_kanade_tracker(images[i], template2, rect2, param_2)        
-        w2 = affine_matrix(param_2)
-        #draw rectange as per updated parameters
-        temp_rect_2 = np.dot(w2,np.vstack((rect2.T, np.ones((1,4))))).T
-        #extract corner elements
-        [xmax2, ymax2] = list(np.max(temp_rect_2, axis = 0).astype(np.int))
-        [xmin2, ymin2] = list(np.min(temp_rect_2, axis = 0).astype(np.int))
-        #create new rectangle/bounding box
-        rect_updated_2 = np.array([[xmin2,ymin2],[xmax2,ymin2],[xmax2,ymax2],[xmin2,ymax2]])        
-        output = cv.polylines(original_frame[i],[rect_updated_2],True,(0,0,255),thickness = 2)
-        vid_output.write(output)
-        cv.imshow("tracker",output)
+
     k = cv.waitKey(20) & 0xFF
     if k == 27:
         break 
